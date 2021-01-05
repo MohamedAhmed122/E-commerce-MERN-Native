93de2aee448e1c8bea54f089249d390c72634623
MohamedAhmed122 <58720977+MohamedAhmed122@users.noreply.github.com>
2021-01-05 17:19:43 +0700

    Connect the App to the db

--- server/server.js
+++ server/server.js
@@ -1,12 +1,24 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import bodyParser from 'body-parser'
+import morgan from 'morgan'
+import colors from 'colors'
+import connectDB from './config/db.js';
 
 
 const app = express()
+app.use(bodyParser.json())
+app.use(morgan('tiny'))
+
+dotenv.config()
+connectDB()
+
+const api = process.env.API_URL
 
 app.get('/',(req,res)=>{
     res.send("Hello From Port 5000")
 })
 
 app.listen(5000, ()=>{
-    console.log('Hello there')
+    console.log('Hello there', api)
 })

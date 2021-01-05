df60caa3c41c1d77f8c6417d5856bbe1163f65c8
MohamedAhmed122 <58720977+MohamedAhmed122@users.noreply.github.com>
2021-01-05 18:24:03 +0700

    Structure the App Router & Controllers & Models

---
+++ server/models/productsModel.js
@@ -0,0 +1,63 @@
+import mongoose from 'mongoose';
+
+const productSchema = mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    description:{
+        type: String,
+        required: true
+    },
+    detail:{
+        type: String,
+       default: ''
+    },
+    image:{
+        type: String,
+        required: true
+    },
+    images:[{type: String}], 
+    brand:{
+        type: String,
+        required: true
+    },
+    price:{
+        type: String,
+        required: true,
+    },
+    countInStock:{
+        type: Number,
+        required: true,
+        default: 0
+    },
+    category:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category',
+        required: true
+    },
+    rating:{
+        type: Number,
+        required: true,
+        default: 0
+    },
+    numReviews:{
+        type: Number,
+        required: true,
+        default: 0
+    },
+    isFeatured:{
+        type: Boolean,
+        default: false
+    },
+    dateCreated:{
+        type: Date,
+        default: Date.now,
+
+    }
+    
+})
+
+const Product = mongoose.model('Product', productSchema);
+
+export default Product;
--- server/server.js
+++ server/server.js
@@ -4,13 +4,20 @@ import bodyParser from 'body-parser'
 import morgan from 'morgan'
 import colors from 'colors'
 import connectDB from './config/db.js';
+import cors from 'cors'
+
+import productRoute from './routes/ProductRoute.js'
 
 
 const app = express()
+
+app.use(cors())
+app.options('*', cors())
 app.use(bodyParser.json())
 app.use(morgan('tiny'))
 
 dotenv.config()
+
 connectDB()
 
 const api = process.env.API_URL
@@ -18,6 +25,11 @@ const api = process.env.API_URL
 app.get('/',(req,res)=>{
     res.send("Hello From Port 5000")
 })
+
+// Routers
+app.use(`${api}/products`, productRoute)
+
+
 
 app.listen(5000, ()=>{
     console.log('Hello there', api)

d27665d8e60f84dccc28dafc6fbe4675ce661f5a
MohamedAhmed122 <58720977+MohamedAhmed122@users.noreply.github.com>
2021-01-07 16:10:47 +0700

    Hashing Password and Adding WebToken

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,28 +1,62 @@
 import User from '../models/userModel.js'
 import asyncHandler from 'express-async-handler'
+import { generateWebToken } from '../utility/GenerateJsonWeTtoken.js';
 
 
 //@desc    Register User 
 //@route   Post /api/user
 //@Access  Public
+// @desc   register new user 
+//@route   GET /api/users
+//@Access  Public
 export const registerUser = asyncHandler (async (req, res) =>{
-
-    let user = await User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-        phone: req.body.phone,
-        isAdmin: req.body.isAdmin,
-        street: req.body.street,
-        zipCode: req.body.zipCode,
-        city: req.body.city,
-        country: req.body.country,
-        apartment: req.body.apartment,
+    const {
+        name,
+        email,
+        password,
+        phone,
+        apartment,
+        street,
+        zipCode,
+        city,
+        country
+    } = req.body;
+ 
+    const userExist = await User.findOne({email})
+ 
+    if(userExist) {
+       res.status(400)
+       throw new Error("User's already exist")
+    }
+ 
+    const user = await User.create({
+       name,
+       email,
+       password,
+       phone,
+       apartment,
+       street,
+       zipCode,
+       city,
+       country
     })
-    
-    const createdUser = await user.save();
-    res.status(201).json(createdUser)
-})
-
-
-
+    if (user){
+       res.status(201).json({
+          id : user._id,
+          email: user.email,
+          name: user.name,
+          isAdmin: user.isAdmin,
+          phone: user.phone,
+          apartment: user.apartment,
+          street: user.street,
+          zipCode: user.zipCode,
+          city: user.city,
+          country: user.country,
+          token: generateWebToken(user._id),
+         
+       })
+    }else{
+       res.status(400)
+       throw new Error('Incorrect Data')
+    }
+ })

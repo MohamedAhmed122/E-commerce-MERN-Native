fbc84f97910c046acfaa9ca9bb707085e24ad1fd
MohamedAhmed122 <58720977+MohamedAhmed122@users.noreply.github.com>
2021-01-08 12:44:26 +0700

    Auth user & get Token

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -3,6 +3,32 @@ import asyncHandler from 'express-async-handler'
 import { generateWebToken } from '../utility/GenerateJsonWeToken.js';
 
 
+
+
+// @desc   Auth user & get Token
+//@route   POST /api/users/login
+//@Access  Public
+export const authUser = asyncHandler (async (req, res) =>{
+   const {email, password} = req.body;
+
+   const user = await User.findOne({email})
+
+   if (user && (await user.matchPassword(password))){
+      res.json({
+         _id : user._id,
+         email: user.email,
+         name: user.name,
+         isAdmin: user.isAdmin,
+         token: generateWebToken(user._id)
+
+      })
+   }else{
+      res.status(401);
+      throw new Error('Invalid Email or/and Password')
+   }
+})
+
+
 //@desc    Register User 
 //@route   Post /api/user
 //@Access  Public
--- server/routes/UserRoute.js
+++ server/routes/UserRoute.js
@@ -1,12 +1,18 @@
 import express from 'express'
 
-import { registerUser, getUserById, getUsers } from '../controllers/UserController.js'
+import { 
+    registerUser,
+    getUserById, 
+    getUsers,
+    authUser,
+ } from '../controllers/UserController.js'
 
 const router = express.Router()
 
 router.route('/').post(registerUser)
 router.route('/').get(getUsers)
 router.route('/:id').get(getUserById)
+router.route('/login').post(authUser)
 
 
 export default router

43002a60c943abac7c4c036aca4e87c879d8c912
MohamedAhmed122 <58720977+MohamedAhmed122@users.noreply.github.com>
2021-01-08 15:44:23 +0700

    Create The Order Model & the OrderItems Model

---
+++ server/models/orderItemModel.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+const orderItemSchema = mongoose.Schema({
+    product:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref : "Product",
+        required: true
+    },
+    quantity:{
+        type: Number,
+        required: true
+    }
+})
+
+const OrderItem = mongoose.model('OrderItem', orderItemSchema);
+
+export default OrderItem;
--- server/routes/CategoryRoute.js
+++ server/routes/CategoryRoute.js
@@ -7,16 +7,16 @@ import {
     getCategoryById,
     updateCategory
 } from '../controllers/CategoryController.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
 
-
 router.route('/').get(getCategory)
 router.route('/:id').get(getCategoryById)
-router.route('/').post(createCategory)
-router.route('/:id').delete(deleteCategory)
-router.route('/:id').put(updateCategory)
+router.route('/').post(protect, admin, createCategory)
+router.route('/:id').delete(protect, admin, deleteCategory)
+router.route('/:id').put(protect, admin, updateCategory)
    
 
 
